a5b47514dae93d7987ab0895b45cb85bcc5bc9a5
MARCO DI CARLO <48115731@ort>
2025-08-19 10:18:11 -0300

    conexion a gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/TP7_CatalogoProductos/'
+  base: '/TP-8---Carrito-de-compras-/'
 })
